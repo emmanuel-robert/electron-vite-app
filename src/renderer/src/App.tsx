6b788692893412b8b74787a93ff9bf2bbf1e1491
Emmanuel Robert <manu.robert@gmail.com>
2024-09-28 00:02:35 +0200

    homepage

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -26,7 +26,7 @@ function App(): JSX.Element {
   return (
     <>
       <img alt="logo" className="logo" src={electronLogo} />
-      <div className="creator">Powered by electron-vite</div>
+      <div className="creator">Powered by electron-vite2</div>
       <div className="text">
         Build an Electron app with <span className="react">React</span>
         &nbsp;and <span className="ts">TypeScript</span>
